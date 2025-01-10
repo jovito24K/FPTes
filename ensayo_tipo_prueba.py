f25cdfda838bb22084e46eec2f6fc1b449336d6b
jovito24k <riquelmemolina2006@gmail.com>
2025-01-10 20:48:00 -0300

    ejercicio tipo prueba 2.0

--- ensayo_tipo_prueba.py
+++ ensayo_tipo_prueba.py
@@ -29,39 +29,41 @@
 #     print(num,"x", i+1,"=",(num)*(i+1))
 
 
-# dinero=0
-# print("HOLA BIENBENIDO")
-# while True:
-#     print("1- ingrese su nombre")
-#     print("2- menu de platos con sus precios")
-#     print("3- mostrar saludo al cliente")
-#     print("4- salir del programa")
-#     opc=int(input("ingre las opciones en orden "))
+dinero=0
+print("HOLA BIENBENIDO")
+while True:
+    print("1- ingrese su nombre")
+    print("2- menu de platos con sus precios")
+    print("3- mostrar saludo al cliente")
+    print("4- salir del programa")
+    opc=int(input("ingre las opciones en orden "))
 
-#     if opc ==1:
-#         nombre=input("INGRESE SU NOMBRE\n")
-#     else:
-#         if opc == 2:
-#             while True:
-#                 print("1- arroz a la francesa   $4500")
-#                 print("2- arroz marinero        $5200")
-#                 print("3- sopa marinera         $9700")
-#                 print("4- salir")
-#                 opc=int(input("elige que plato deseas\n"))
-#                 if opc == 1:
-#                     dinero=dinero+4500
-#                 if opc == 2:
-#                     dinero=dinero+5200
-#                 if opc == 3:
-#                     dinero=dinero+9700
-#                 if opc == 4:
-#                     ("salir")
-#                     break
-#         else:
-#             if opc ==3:
-#                 print(f"gracias {nombre} por venir al restorant panuchis su total es {dinero}")
-#             else:
-#                 if opc == 4 :
-#                     print("saliendo del programa")
-#                 break
+    if opc ==1:
+        nombre=input("INGRESE SU NOMBRE\n")
+    else:
+        if opc == 2:
+            while True:
+                print("1- arroz a la francesa   $4500")
+                print("2- arroz marinero        $5200")
+                print("3- sopa marinera         $9700")
+                print("4- salir")
+                opc=int(input("elige que plato deseas\n"))
+                if opc == 1:
+                    dinero=dinero+4500
+                if opc == 2:
+                    dinero=dinero+5200
+                if opc == 3:
+                    dinero=dinero+9700
+                if opc == 4:
+                    ("salir")
+                    break
+                else:
+                    print("la opcion no esta disponible")      
+        else:
+            if opc ==3:
+                print(f"gracias {nombre} por venir al restorant panuchis su total es {dinero}")
+            else:
+                if opc == 4 :
+                    print("saliendo del programa")
+                break
 
