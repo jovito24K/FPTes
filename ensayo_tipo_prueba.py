13c0a0494e97530c414cc48e00980225c9c117dd
jovito24k <riquelmemolina2006@gmail.com>
2025-01-10 20:40:50 -0300

    ejercicio tipo prueba

---
+++ ensayo_tipo_prueba.py
@@ -0,0 +1,67 @@
+# from os import system
+# system("cls")
+# comprar=int(input("cuanos años tienes ?\n"))
+# if comprar >10:
+#     print ("el ticket cuenta $1000")
+# else:
+#     if comprar > 18 and comprar <65:
+#         print ("el ticket cuensta $2000")
+#     else:
+#         if comprar> 65:
+#             print("el ticket cuesta 1500")
+#         else:
+#             print("el ticket es gratis")
+
+
+# num1=int(input("ingrese un numero\n"))
+# num2=int(input("ingrese el segundo nuemro\n"))
+# if num1 > num2 :
+#     print (f"el numero mayo es {num1} ")
+# else:
+#     print(f"el nuemro mayor es {num2}")
+
+
+
+
+# TABLAS DE MULTIPLICAR
+# num=int(input("ingrese el numero de la tabla que quiere ver\n"))
+# for i in range(10):
+#     print(num,"x", i+1,"=",(num)*(i+1))
+
+
+# dinero=0
+# print("HOLA BIENBENIDO")
+# while True:
+#     print("1- ingrese su nombre")
+#     print("2- menu de platos con sus precios")
+#     print("3- mostrar saludo al cliente")
+#     print("4- salir del programa")
+#     opc=int(input("ingre las opciones en orden "))
+
+#     if opc ==1:
+#         nombre=input("INGRESE SU NOMBRE\n")
+#     else:
+#         if opc == 2:
+#             while True:
+#                 print("1- arroz a la francesa   $4500")
+#                 print("2- arroz marinero        $5200")
+#                 print("3- sopa marinera         $9700")
+#                 print("4- salir")
+#                 opc=int(input("elige que plato deseas\n"))
+#                 if opc == 1:
+#                     dinero=dinero+4500
+#                 if opc == 2:
+#                     dinero=dinero+5200
+#                 if opc == 3:
+#                     dinero=dinero+9700
+#                 if opc == 4:
+#                     ("salir")
+#                     break
+#         else:
+#             if opc ==3:
+#                 print(f"gracias {nombre} por venir al restorant panuchis su total es {dinero}")
+#             else:
+#                 if opc == 4 :
+#                     print("saliendo del programa")
+#                 break
+
